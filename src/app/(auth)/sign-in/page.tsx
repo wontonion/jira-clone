49ff95e22eb156f8777f666f94aec1078184e2b6
wontonion <chu_yisheng@hotmail.com>
2024-11-29 17:27:08 -0500

    make sign in page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import { SignInCard } from '@/features/components/sign-in-card'
+
+const SignInPage = () => {
+  return <SignInCard /> 
+  
+}
+
+export default SignInPage
