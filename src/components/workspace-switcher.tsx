4f03989b40f6ef0a19ff5dc74be67d4358a9fe4e
wontonion <chu_yisheng@hotmail.com>
2024-12-31 18:27:59 -0500

    create workspace member

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -4,17 +4,23 @@ import { RiAddCircleFill } from "react-icons/ri"
 import { useWorkspaces } from "@/features/workspaces/api/use-get-workspaces"
 import { Select, SelectItem, SelectContent, SelectTrigger, SelectValue } from "./ui/select"
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar"
+import { useRouter } from "next/navigation"
 
 export const WorkspaceSwitcher = () => { 
+    const router = useRouter()
     const { data: workspaces } = useWorkspaces()
     
+    const onSelect = (workspaceId: string) => {
+        router.push(`/workspaces/${workspaceId}`)
+    }
+
     return (
         <div className="flex flex-col gap-y-2">
             <div className="flex items-center justify-between">
                 <p className="text-xs uppercase text-neutral-500">Workspaces</p>
                 <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
             </div>
-            <Select>
+            <Select onValueChange={onSelect}>
                 <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
                     <SelectValue placeholder="No workspaces selected" />
                 </SelectTrigger>
