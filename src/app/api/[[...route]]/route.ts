7a13f22e7e91a5449d3b4a6b932a65b2570a4e6f
wontonion <chu_yisheng@hotmail.com>
2024-12-31 11:19:20 -0500

    add workspace route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel"
 import auth from "@/features/auth/server/route"
-
+import workspaces from "@/features/workspaces/server/route"
 const app = new Hono().basePath("/api");
 
 const routes = app
-    .route("/auth", auth);
+    .route("/auth", auth)
+    .route("/workspaces", workspaces)
 
 // export for Vercel
 export const GET = handle(app)
