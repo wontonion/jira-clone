97f94c9d685c73016554cc7f2540bab8509078f3
wontonion <chu_yisheng@hotmail.com>
2025-01-03 11:33:22 -0500

    build member system

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,6 +1,6 @@
 import "server-only"
 
-import { Client, Account, Databases } from 'node-appwrite';
+import { Client, Account, Databases, Users } from 'node-appwrite';
 import { cookies } from "next/headers";
 import { AUTH_COOKIE } from "@/features/auth/constants";
 
@@ -38,6 +38,9 @@ export async function createAdminClient() {
     return {
         get account() {
             return new Account(client);
-        }
+        },
+        get users() {
+            return new Users(client);
+        },
     }
 }
